450630c8cfa14a912ff2212a37f1cf598e4d6216
Lambda_School_Loaner_229 <lambda_school_loaner_229@Lambda-School-Loaner-229s-MacBook-Pro.local>
2020-01-17 22:05:56 -0700

    Added Axios dependency, imported Github user object via url (Step 1).

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App!</h1>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+       userData: {}
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://api.github.com/users/jzanderson09')
+    .then(res => this.setState({ userData: res.data }))
+    .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>App!</h1>
+        <div className='App-container'>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
